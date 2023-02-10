648e3f483ad947f591465cc0d58a7fbc8afd91c8
Rodrigo Vargas <rodrigovargas123@Rodrigos-MacBook-Pro.local>
2023-02-10 17:07:56 -0300

    first commit

---
+++ config/plugins.ts
@@ -0,0 +1,19 @@
+module.exports = () => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: ('dtdjv6zdl'),
+        api_key: ('743911189584111'),
+        api_secret: ('rt9h2T04PTnfT8z8-ueB6vwqDX0'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
